2adbdd9a9af6e2aa1b4d35caffdfa28db28ec4ce
Ярослав <yaroslav@MBP-Aroslav.lan>
2023-10-15 00:14:56 +0300

    second

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ let a = '';
 let b = '';
 let sign ='';
 let finish = false;
+inputElement.setAttribute("value", 0);
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', '/', 'x'];
+
+function clearAll(){
+  a = '';
+  b = '';
+  sign = '';
+  finish = false;
+  inputElement.setAttribute("value", 0);
+}
+
+document.querySelector('.calc_numb_ac').onclick = clearAll
 
 buttons.forEach(function(buttons){
   buttons.addEventListener("click", function(){
@@ -16,6 +27,9 @@ buttons.forEach(function(buttons){
       if(b === '' && sign === ''){
         a+=buttonValue
         inputElement.setAttribute("value", a);
+      } 
+      else if(a!=='' && b!=='' && finish){
+        
       }
       else {
         b += buttonValue
@@ -23,6 +37,28 @@ buttons.forEach(function(buttons){
       }
       console.log(a, b, sign)
       return; 
+    }
+
+
+    if(buttonValue === '='){
+      if (b === '') b = a;
+      switch (sign) {
+        case "+":
+          a = (+a)+(+b) 
+          break;
+        case "-":
+          a = a - b 
+        break;
+        case "*":
+          a = a * b 
+        break;
+        case "/":
+          a = a / b 
+        break;
+      }
+      finish=true
+      inputElement.setAttribute("value", a)
+      console.log(a, b, sign)
     }
 
     if (action.includes(buttonValue)){
